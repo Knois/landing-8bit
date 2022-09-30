ba68f76d183716c94882c6e7199c5044282aea47
Knois <knoisknois@gmail.com>
2022-09-30 15:03:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="header__text">RISING FROM THE ASHES</div>
+      </div>
+      <a
+        className="room"
+        href="https://oncyber.io/explore"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="room__text">PLEASANT</div>
+        <div className="room__text">VOID</div>
+        <div className="room__text">ENTRY</div>
+      </a>
+      <div className="scroll">
+        <div className="scroll__chapter">CHAPTER ALPHA</div>
+        <div className="scroll__box">
+          <div className="scroll__box__string">
+            <a
+              className="scroll__text"
+              href="https://opensea.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              PET THE VOID
+            </a>
+            <a
+              className="scroll__icon_one"
+              href="https://opensea.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+            </a>
+          </div>
+          <div className="scroll__box__string">
+            <a
+              className="scroll__text"
+              href="https://opensea.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              FIND NEW CANVAS
+            </a>
+            <a
+              className="scroll__icon_two"
+              href="https://opensea.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
